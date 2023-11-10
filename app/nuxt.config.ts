01353812dd033a1a8871c19fbb8b0bb57149fde4
Ryan-santos <ryandossantosfelix22@gmail.com>
2023-11-10 17:59:55 -0300

    :lipstick: feat: Preparando estilização

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,9 +1,35 @@
 export default defineNuxtConfig({
+    css: [
+        "~/assets/scss/main.scss"
+    ],
+
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: [
+                        "@use \"@/assets/scss/colors.scss\" as *;",
+                        "@use \"@/assets/scss/spacings.scss\" as *;",
+                        "@use \"@/assets/scss/screens.scss\" as *;"
+                    ].join("\n")
+                }
+            }
+        }
+    },
+
     devtools: {
         enabled: true
     },
 
     modules: [
+        "@nuxtjs/google-fonts",
         "@pinia/nuxt"
-    ]
+    ],
+
+    googleFonts: {
+        download: false,
+        families: {
+            Poppins: [100, 200, 300, 400, 500, 600, 700, 800, 900]
+        }
+    }
 });
